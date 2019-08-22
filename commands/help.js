6fb09558aeda744bd69efeda6df5582d3e9ef38c
HuckinB <huckin.brandon@gmail.com>
2019-08-22 20:44:19 +0100

    help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
             command = bot.commands.get(command);
             var SHembed = new Discord.RichEmbed()
             .setColor(colours.cyan)
-            .setAuthor(`TestBOT HELP`, message.guild.iconURL)
+            .setAuthor(`Huckin-Bot HELP`, message.guild.iconURL)
             .setThumbnail(bot.user.displayAvatarURL)
             .setDescription(`The bot prefix is: ${prefix}\n\n**Command:** ${command.help.name}\n**Description:** ${command.help.description || "No Description"}\n**Usage:** ${command.help.usage || "No Usage"}\n**Accessable by:** ${command.help.accessableby || "Members"}\n**Aliases:** ${command.help.noalias || command.help.aliases}`)
             message.channel.send(SHembed);
@@ -29,15 +29,16 @@ module.exports.run = async (bot, message, args) => {
 
         let Sembed = new Discord.RichEmbed()
         .setColor(colours.cyan)
-        .setAuthor(`TestBOT Help`, message.guild.iconURL)
+        .setAuthor(`Huckin-Bot Help`, message.guild.iconURL)
         .setThumbnail(bot.user.displayAvatarURL)
         .setTimestamp()
-        .setDescription(`These are the avaliable commands for the TestBOT!\nThe bot prefix is: ${prefix}`)
-        .addField(`Commands:`, "``cat`` ``dog`` ``meme`` ``help`` ``serverinfo`` ``userinfo``")
-        .setFooter("Test Bot 2k18", bot.user.displayAvatarURL)
+        .setDescription(`These are the avaliable commands for the ${bot.user.username}!\nThe bot prefix is: ${prefix}`)
+        .addField(`Commands:`, "``botinfo`` ``help`` ``report`` ``serverinfo`` ``steam`` ``suggest`` ``uptime`` ``userinfo``")
+        .setFooter("Huckin-Bot", bot.user.displayAvatarURL)
         message.channel.send(embed).then(m => m.delete(10000));
         message.author.send(Sembed)
     }
+    message.delete().catch();
 }
 
 
